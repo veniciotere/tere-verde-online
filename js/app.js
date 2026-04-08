40baff21b381440ed8ed9c530d0dc297b04d1164
veniciotere <venicio.tere@gmail.com>
2026-04-08 19:31:44 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+function salvarEvento() {
+    const nome = document.getElementById("eventoNome").value;
+    const data = document.getElementById("eventoData").value;
 
+    let eventos = JSON.parse(localStorage.getItem("eventos")) || [];
+
+    eventos.push({ nome, data });
+
+    localStorage.setItem("eventos", JSON.stringify(eventos));
+
+    listarEventos();
+}
+
+function listarEventos() {
+    const lista = document.getElementById("listaEventos");
+    lista.innerHTML = "";
+
+    let eventos = JSON.parse(localStorage.getItem("eventos")) || [];
+
+    eventos.forEach(e => {
+        const li = document.createElement("li");
+        li.innerText = `${e.nome} - ${e.data}`;
+        lista.appendChild(li);
+    });
+}
+
+window.onload = listarEventos;
