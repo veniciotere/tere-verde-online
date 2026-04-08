10f253f8d8de2332e7ab6953e7ab819c5f340568
veniciotere <venicio.tere@gmail.com>
2026-04-08 20:06:00 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,14 @@
+window.onload = function () {
+    checkAuth();
+
+    document.getElementById("usuarioInfo").innerText =
+        "Logado como: " + localStorage.getItem("usuarioLogado");
+
+    listarUsuarios();
+    listarEventosAdmin();
+};
+
+// CRIAR USUÁRIO
 function criarUsuario() {
     cadastrarUsuario(
         document.getElementById("novoUsuario").value,
@@ -6,9 +17,11 @@ function criarUsuario() {
     );
 }
 
-// Executa ao carregar
-window.onload = function () {
-    checkAuth();
-    listarUsuarios();
-    listarEventosAdmin();
-};
+// CRIAR EVENTO
+function criarEvento() {
+    adicionarEvento(
+        document.getElementById("nome").value,
+        document.getElementById("data").value,
+        "Ativo"
+    );
+}
