9d31432a56df459f823dd313bb2f083b0efb9d3f
veniciotere <venicio.tere@gmail.com>
2026-04-08 19:30:42 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,27 @@
+// Usuário fake (admin)
+const adminUser = {
+    email: "admin@tereverde.com",
+    senha: "123456"
+};
 
+function login(email, senha) {
+    if (email === adminUser.email && senha === adminUser.senha) {
+        localStorage.setItem("logado", "true");
+        window.location.href = "admin.html";
+    } else {
+        alert("Login inválido!");
+    }
+}
+
+function verificarLogin() {
+    const logado = localStorage.getItem("logado");
+
+    if (!logado) {
+        window.location.href = "login.html";
+    }
+}
+
+function logout() {
+    localStorage.removeItem("logado");
+    window.location.href = "index.html";
+}
